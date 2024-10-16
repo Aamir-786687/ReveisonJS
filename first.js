da5adf0b2b3e7decc3a18c57baaf62357d7bdfef
Aamir <aamir557ahmad@gmail.com>
2024-10-16 13:04:07 +0530

    added

--- first.js
+++ first.js
@@ -1,87 +1,70 @@
-// console.log("Question 11");
-// let temp=[32,31,30,29,28,27,26];
+console.log("Question 11");
+let temp=[32,31,30,29,28,27,26];
 
-// temp[3]= temp[3]+2
+temp[3]= temp[3]+2
 
-// let updatedtemp= temp+""
-//   console.log("Updated Temperature:")
-//   console.log(updatedtemp);
-
-
-
-//   console.log("");
-//   console.log("");
+let updatedtemp= temp+""
+  console.log("Updated Temperature:")
+  console.log(updatedtemp);
 
 
-//   console.log("Question 12");
-//   let todoList = ["Bathing", "Eating", "Exercise", "Classes", "Walking"] 
+
+  console.log("");
+  console.log("");
+
+
+  console.log("Question 12");
+  let todoList = ["Bathing", "Eating", "Exercise", "Classes", "Walking"] 
    
-//    todoList.unshift("Brushing");
+   todoList.unshift("Brushing");
    
-//    todoList.pop();
+   todoList.pop();
    
-//    console.log(todoList);
+   console.log(todoList);
 
 
 
-//    console.log("");
-//    console.log("");
+   console.log("");
+   console.log("");
 
 
-//  console.log("Question 13");
-//  let pendingTasks = [ "Check emails", "Attend team meeting","Finish project report","Update task management system",
-//     "Prepare presentation for tomorrow"]
+ console.log("Question 13");
+ let pendingTasks = [ "Check emails", "Attend team meeting","Finish project report","Update task management system",
+    "Prepare presentation for tomorrow"]
     
     
-//        pendingTasks.shift();
+       pendingTasks.shift();
        
-//        pendingTasks[4] = ("Submitting the reports")
+       pendingTasks[4] = ("Submitting the reports")
        
-//        console.log(pendingTasks);
+       console.log(pendingTasks);
 
 
-//     console.log("");
-//     console.log("");
+    console.log("");
+    console.log("");
 
-//     console.log("Question 14");
-//     let studentScore= [36,54,78,19,65,99,18.90,97,37];
+    console.log("Question 14");
+    let studentScore= [36,54,78,19,65,99,18.90,97,37];
  
-//  for (let i=0; i<studentScore.length; i++){
-//   if(studentScore[i]<40){
-//      studentScore[i]+=20
-//   } else if (studentScore[i]>90){
-//     studentScore[i]=90
-//   }
-// }
-// let passedStudents=0
-// for(let i=0; i<studentScore.length; i++){
-//   if (studentScore[i]>=50){
-//     passedStudents++
-//   }
-// }
-// console.log("Number of Students Passed:",passedStudents)
+ for (let i=0; i<studentScore.length; i++){
+  if(studentScore[i]<40){
+     studentScore[i]+=20
+  } else if (studentScore[i]>90){
+    studentScore[i]=90
+  }
+}
+let passedStudents=0
+for(let i=0; i<studentScore.length; i++){
+  if (studentScore[i]>=50){
+    passedStudents++
+  }
+}
+console.log("Number of Students Passed:",passedStudents)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log("");
+    console.log("");
 
 function checkColor() {
   let inputColor = prompt("Enter a color:");  
